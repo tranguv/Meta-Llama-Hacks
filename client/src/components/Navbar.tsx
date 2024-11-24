2d2af5883bf9e9acfa394749bfd4d491a8dd96a4
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-11-24 11:33:41 -0500

    Done with FrontEnd Revamp

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Navbar = () => {
     return (
-        <div className="fixed top-0 h-[8%] z-10 w-full bg-[#FFB302] flex flex-row justify-between items-center px-10"></div>
+        <div className="fixed top-0 h-[18%] w-full flex items-center justify-center text-[#222366]">
+            <h1 className="font-serif
+ text-[80px]">Llama Care</h1>
+        </div>
     );
 };
 
