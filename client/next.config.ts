38cc78a4a90236c34476865d8ba759c8c463de6d
tranguv <89886131+tranguv@users.noreply.github.com>
2024-11-24 12:58:18 -0500

    deploy

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => [
+    {
+      source: "/ask-all/",
+      destination: process.env.NODE_ENV === "development" ? `${process.env.NEXT_PUBLIC_MODEL_API}/ask-all/` : "/ask-all/",
+    },
+  ],
   images: {
     formats: ["image/avif", "image/webp"], // Add WebP format support
   },
